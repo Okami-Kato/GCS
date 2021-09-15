aac1e02cba925500dd09560add08f9c39b37ebb4
Okami-Kato <barkan.makar@gmail.com>
2021-09-15 18:08:28 +0300

    renamed to DaoConfig

--- dao/src/main/java/com/epam/esm/dao/config/DaoConfig.java
+++ dao/src/main/java/com/epam/esm/dao/config/DaoConfig.java
@@ -2,14 +2,18 @@ package com.epam.esm.dao.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@ComponentScan("com.epam.esm.dao")
+@EntityScan("com.epam.esm.entity")
 @EnableTransactionManagement
-public class PersistenceJPAConfig {
+public class DaoConfig {
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(PersistenceJPAConfig.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(DaoConfig.class, args);
         System.out.println("lol");
     }
 }
