5162c6b099b4c6351150c0385764f30ae6544ba7
Okami-Kato <barkan.makar@gmail.com>
2021-09-15 18:09:46 +0300

    created CertificateDaoImpl

---
+++ dao/src/main/java/com/epam/esm/dao/impl/CertificateDaoImpl.java
@@ -0,0 +1,64 @@
+package com.epam.esm.dao.impl;
+
+import com.epam.esm.dao.CertificateDao;
+import com.epam.esm.entity.Certificate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@Transactional
+public class CertificateDaoImpl implements CertificateDao {
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public void addTag(Integer certificateId, Integer tagId) {
+
+    }
+
+    @Override
+    public boolean removeTag(Integer certificateId, Integer tagId) {
+        return false;
+    }
+
+    @Override
+    public List<Certificate> getAllByTags(Integer... ids) {
+        return null;
+    }
+
+    @Override
+    public Optional<Certificate> get(Integer id) {
+        return Optional.ofNullable(manager.find(Certificate.class, id));
+    }
+
+    @Override
+    public List<Certificate> getAll() {
+        return null;
+    }
+
+    @Override
+    public Certificate create(Certificate certificate) {
+        manager.persist(certificate);
+        return certificate;
+    }
+
+    @Override
+    public void update(Certificate certificate) {
+        manager.merge(certificate);
+    }
+
+    @Override
+    public boolean delete(Integer id) {
+        return false;
+    }
+
+    @Override
+    public boolean idExists(Integer id) {
+        return false;
+    }
+}
