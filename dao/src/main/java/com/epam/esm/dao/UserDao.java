c0070e3f1f00e09e66f0f531d137fcbe88aa862e
Okami-Kato <barkan.makar@gmail.com>
2021-09-21 12:43:36 +0300

    created UserDao

---
+++ dao/src/main/java/com/epam/esm/dao/UserDao.java
@@ -0,0 +1,7 @@
+package com.epam.esm.dao;
+
+import com.epam.esm.entity.User;
+
+public interface UserDao extends Dao<User, Integer> {
+
+}
---
+++ dao/src/main/java/com/epam/esm/dao/impl/UserDaoImpl.java
@@ -0,0 +1,69 @@
+package com.epam.esm.dao.impl;
+
+import com.epam.esm.dao.UserDao;
+import com.epam.esm.entity.Tag;
+import com.epam.esm.entity.User;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityGraph;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+@Transactional
+public class UserDaoImpl implements UserDao {
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public Optional<User> get(Integer id) {
+        EntityGraph<?> graph = manager.getEntityGraph("graph.user.orders");
+        Map<String, Object> hints = new HashMap<>();
+        hints.put("javax.persistence.fetchgraph", graph);
+
+        return Optional.ofNullable(manager.find(User.class, id, hints));
+    }
+
+    @Override
+    public List<User> getAll(int pageNumber, int pageSize) {
+        TypedQuery<Integer> idQuery = manager.createQuery("SELECT u.id FROM User u ORDER BY u.id", Integer.class);
+        List<Integer> userIds = idQuery
+                .setFirstResult((pageNumber - 1) * pageSize)
+                .setMaxResults(pageSize)
+                .getResultList();
+
+        EntityGraph<?> graph = manager.getEntityGraph("graph.user.orders");
+
+        TypedQuery<User> userQuery = manager.createQuery("SELECT u FROM User u WHERE u.id in (:ids)", User.class);
+        return userQuery
+                .setParameter("ids", userIds)
+                .setHint("javax.persistence.fetchgraph", graph)
+                .getResultList();
+    }
+
+    @Override
+    public long getCount() {
+        return manager.createQuery("SELECT COUNT(u) FROM User u", Long.class).getSingleResult();
+    }
+
+    @Override
+    public void create(User user) {
+        throw new UnsupportedOperationException("Method create() isn't supported in UserDaoImpl");
+    }
+
+    @Override
+    public void update(User user) {
+        throw new UnsupportedOperationException("Method update() isn't supported in UserDaoImpl");
+    }
+
+    @Override
+    public void delete(Integer id) {
+        throw new UnsupportedOperationException("Method delete() isn't supported in UserDaoImpl");
+    }
+}
