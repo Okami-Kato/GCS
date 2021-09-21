0cedde21fe508933fe54004d333259c4226303ba
Okami-Kato <barkan.makar@gmail.com>
2021-09-21 13:24:40 +0300

    created userDao tests

---
+++ dao/src/test/java/com/epam/esm/dao/impl/UserDaoImplTest.java
@@ -0,0 +1,57 @@
+package com.epam.esm.dao.impl;
+
+import com.epam.esm.dao.UserDao;
+import com.epam.esm.dao.config.DaoConfig;
+import com.epam.esm.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(classes = DaoConfig.class)
+@ActiveProfiles("test")
+class UserDaoImplTest {
+    private final User user = new User("Shannon", "Ramsay");
+
+    @Autowired
+    UserDao userDao;
+
+    @Test
+    @Sql({"classpath:sql/test-user-data.sql"})
+    void getAll() {
+        int count = (int) userDao.getCount();
+        assertDoesNotThrow(() -> userDao.getAll(1, count));
+        assertEquals(count, userDao.getAll(1, count + 1).size());
+        assertThrows(InvalidDataAccessApiUsageException.class, () -> userDao.getAll(-1, 10));
+        assertThrows(InvalidDataAccessApiUsageException.class, () -> userDao.getAll(1, -10));
+
+        User user = userDao.getAll(1, 1).get(0);
+        assertDoesNotThrow(() -> userDao.get(user.getId()));
+        assertTrue(userDao.get(user.getId()).isPresent());
+        assertFalse(userDao.get(user.getId() * (-1)).isPresent());
+        assertThrows(InvalidDataAccessApiUsageException.class, () -> userDao.get(null));
+    }
+
+    @Test
+    void create() {
+        assertThrows(UnsupportedOperationException.class, () -> userDao.create(user));
+    }
+
+    @Test
+    void update() {
+        assertThrows(UnsupportedOperationException.class, () -> userDao.update(user));
+    }
+
+    @Test
+    void delete() {
+        assertThrows(UnsupportedOperationException.class, () -> userDao.delete(user.getId()));
+    }
+}
---
+++ dao/src/test/resources/sql/test-user-data.sql
@@ -0,0 +1,10 @@
+INSERT INTO user
+VALUES (1, 'Daniele', 'Hills');
+INSERT INTO user
+VALUES (2, 'Amy', 'Baldwin');
+INSERT INTO user
+VALUES (3, 'Kajal', 'Mosley');
+INSERT INTO user
+VALUES (4, 'Rahima', 'Suarez');
+INSERT INTO user
+VALUES (5, 'Arvin', 'Wolfe');
