d2c4a52d4d4da21ff5ca8cc56ffde8496f13f635
Okami-Kato <barkan.makar@gmail.com>
2021-09-28 17:14:12 +0300

    mapper Instant to LocalDateTime converter

--- service/src/main/java/com/epam/esm/service/config/ServiceConfig.java
+++ service/src/main/java/com/epam/esm/service/config/ServiceConfig.java
@@ -1,9 +1,17 @@
 package com.epam.esm.service.config;
 
+import com.epam.esm.entity.Certificate;
+import com.epam.esm.service.dto.response.CertificateResponse;
+import org.modelmapper.Condition;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Configuration
 @ComponentScan("com.epam.esm.service")
@@ -11,8 +19,14 @@ public class ServiceConfig {
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper mapper = new ModelMapper();
-        mapper.getConfiguration()
-                .setSkipNullEnabled(true);
+        mapper.getConfiguration().setSkipNullEnabled(true);
+
+        Condition<?, ?> notNull = ctx -> ctx.getSource() != null;
+        Converter<Instant, LocalDateTime> toLocalDate = ctx -> LocalDateTime.ofInstant(ctx.getSource(), ZoneId.systemDefault());
+
+        mapper.typeMap(Certificate.class, CertificateResponse.class)
+                .addMappings(m -> m.when(notNull).using(toLocalDate).map(Certificate::getCreateDate, CertificateResponse::setCreateDate))
+                .addMappings(m -> m.when(notNull).using(toLocalDate).map(Certificate::getLastUpdateDate, CertificateResponse::setLastUpdateDate));
         return mapper;
     }
 }
--- service/src/test/java/com/epam/esm/service/impl/TestServiceConfig.java
+++ service/src/test/java/com/epam/esm/service/impl/TestServiceConfig.java
@@ -1,16 +1,30 @@
 package com.epam.esm.service.impl;
 
+import com.epam.esm.entity.Certificate;
+import com.epam.esm.service.dto.response.CertificateResponse;
+import org.modelmapper.Condition;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @SpringBootConfiguration
 public class TestServiceConfig {
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper mapper = new ModelMapper();
-        mapper.getConfiguration()
-                .setSkipNullEnabled(true);
+        mapper.getConfiguration().setSkipNullEnabled(true);
+
+        Condition<?, ?> notNull = ctx -> ctx.getSource() != null;
+        Converter<Instant, LocalDateTime> toLocalDate = ctx -> LocalDateTime.ofInstant(ctx.getSource(), ZoneId.systemDefault());
+
+        mapper.typeMap(Certificate.class, CertificateResponse.class)
+                .addMappings(m -> m.when(notNull).using(toLocalDate).map(Certificate::getCreateDate, CertificateResponse::setCreateDate))
+                .addMappings(m -> m.when(notNull).using(toLocalDate).map(Certificate::getLastUpdateDate, CertificateResponse::setLastUpdateDate));
         return mapper;
     }
 }
