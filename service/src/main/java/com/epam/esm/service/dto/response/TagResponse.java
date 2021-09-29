c189b275e384ddd030a7cee536bf331603b4a6c6
Okami-Kato <barkan.makar@gmail.com>
2021-09-29 16:14:48 +0300

    rearranged code

--- service/src/main/java/com/epam/esm/service/dto/response/TagResponse.java
+++ service/src/main/java/com/epam/esm/service/dto/response/TagResponse.java
@@ -5,19 +5,19 @@ import java.util.Objects;
 public class TagResponse extends AbstractResponse {
     private String name;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public TagResponse() {
     }
 
     public TagResponse(Integer id, String name) {
         super(id);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
